0fce91187ee7e71ef85632e9a0a78879651c3cc3
Whizkelvin <kelvinmelchizedek@gmail.com>
2024-01-22 22:08:21 +0000

    update 3

--- src/index.js
+++ src/index.js
@@ -6,21 +6,14 @@ let closeBtn =  document.getElementById('close-btn')
 hamburger_Menu.addEventListener('click', displayMenu)
 function displayMenu (){
     navLinks.style.display = 'block'
-    hamburger_Menu.style.display = 'none'
-    closeBtn.style.display = 'block'
-    hamburger_Menu.style.display = 'none'
 }
 
 navLinks.addEventListener('click', displayClose)
 function displayClose (){
     navLinks.style.display = 'none'
-    hamburger_Menu.style.display = "block"
-    closeBtn.style.display = 'none'
 }
 
 closeBtn.addEventListener('click', closeMenu)
 function closeMenu (){
     navLinks.style.display = 'none'
-    hamburger_Menu.style.display = 'block'
-    closeBtn.style.display = 'none'
 }
